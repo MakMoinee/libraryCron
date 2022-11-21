3e6926b32ee4a7bb7c3710da004032cec17bfd1a
DESKTOP-LKF81KV\Mak Moinee <titus.silver.ph@gmail.com>
2022-11-21 12:00:36 +0800

    update code

---
+++ LibraryCron/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.cbRunBG = New System.Windows.Forms.CheckBox()
+        Me.btnSaveSetting = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'cbRunBG
+        '
+        Me.cbRunBG.AutoSize = True
+        Me.cbRunBG.Location = New System.Drawing.Point(43, 42)
+        Me.cbRunBG.Name = "cbRunBG"
+        Me.cbRunBG.Size = New System.Drawing.Size(127, 19)
+        Me.cbRunBG.TabIndex = 0
+        Me.cbRunBG.Text = "Run In Background"
+        Me.cbRunBG.UseVisualStyleBackColor = True
+        '
+        'btnSaveSetting
+        '
+        Me.btnSaveSetting.Location = New System.Drawing.Point(29, 98)
+        Me.btnSaveSetting.Name = "btnSaveSetting"
+        Me.btnSaveSetting.Size = New System.Drawing.Size(164, 32)
+        Me.btnSaveSetting.TabIndex = 1
+        Me.btnSaveSetting.Text = "Save Setting"
+        Me.btnSaveSetting.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(210, 142)
+        Me.Controls.Add(Me.btnSaveSetting)
+        Me.Controls.Add(Me.cbRunBG)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents cbRunBG As CheckBox
+    Friend WithEvents btnSaveSetting As Button
+End Class
---
+++ LibraryCron/Form1.vb
@@ -0,0 +1,213 @@
+﻿Imports Microsoft.Win32
+
+Public Class Form1
+
+    Private runInBG As Boolean = False
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim runbg = getConfiguration()
+        If runbg Then
+            cbRunBG.CheckState = CheckState.Checked
+            Me.Hide()
+            loadService()
+
+        Else
+            cbRunBG.CheckState = CheckState.Unchecked
+        End If
+
+    End Sub
+
+    Private Sub btnSaveSetting_Click(sender As Object, e As EventArgs) Handles btnSaveSetting.Click
+        MsgBox("If run in background is checked the cronjob will start and this form will hide", vbInformation)
+        Dim query = String.Format(updateCronSettings, runInBG)
+        Dim isUpdate = updateQuery(query)
+        If isUpdate Then
+        Else
+            runInBG = False
+            MsgBox("Failed To Update, Can't run the cron as of the moment, Please seek help from your system administrator", vbCritical)
+        End If
+        loadService()
+
+    End Sub
+
+    Private Sub loadService()
+        If runInBG Then
+            Me.Hide()
+
+            Dim regKey As RegistryKey
+            regKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+            regKey.SetValue("LibraryCronJob", "D:\LibraryCron\LibraryCron\bin\Debug\net6.0-windows\LibraryCron.exe /background")
+            regKey.Close()
+
+            Dim seconds As Integer = 0
+
+            Do
+                If seconds.Equals(10000) Then
+                    Dim runbg = getConfiguration()
+                    If runbg <> True Then
+                        runInBG = False
+                        cbRunBG.CheckState = CheckState.Unchecked
+                        Me.Show()
+                        Exit Do
+                    End If
+                    seconds = 0
+                    Dim list As List(Of ReturnBooks) = getBorrowData()
+                    For i As Integer = 0 To list.Count - 1
+                        Dim rBook As ReturnBooks = list(i)
+                        If rBook.getContactNumber() = "" Then
+                            Continue For
+                        End If
+
+
+
+                        Dim exist As Boolean = checkIfMsgExist(rBook.getBookNumber(), rBook.getBorrowerName())
+
+                        If exist Then
+                            Continue For
+                        End If
+
+                        If rBook.getCategory().ToLower().Equals("textbook") Then
+                            Dim dets As New Date
+                            Date.TryParse(rBook.getDateToReturn, dets)
+                            Dim betweenDays = DateAndTime.DateDiff(DateInterval.Day, Date.Parse(Date.Now().ToShortDateString()), dets)
+                            If betweenDays = 1 Then
+                                Dim message = String.Format(smsTemplateTomorrow, rBook.getBorrowerName, rBook.getBookTitle, rBook.getOriginalDateToReturn)
+                                Try
+                                    Dim result = SMP(smsApiKey, rBook.getContactNumber, Message, "SEMAPHORE")
+                                    insert(String.Format(insertCronSentQuery, rBook.getBookNumber, rBook.getBorrowerName, rBook.getDateToReturn))
+                                Catch ex As Exception
+                                    ' MsgBox(ex.Message)
+                                End Try
+                            End If
+
+                        End If
+                        If rBook.getCategory().ToLower().Equals("general reference") Then
+                            Try
+                                Dim sliceTime = rBook.getOriginalDateToReturn().Split("-")
+                                Dim hoursAndMinutes = sliceTime(1).Split(":")
+                                Dim hours As String = hoursAndMinutes(0).Trim()
+                                Dim sliceMins = hoursAndMinutes(1).Split(" ")
+                                Dim amPm = sliceMins(1).Trim()
+                                Dim mins = sliceMins(0).Trim()
+                                If mins = "00" Then
+                                    mins = "00"
+                                End If
+                                If amPm.ToLower().Equals("pm") Then
+                                    hours = hours + 12
+                                End If
+
+                                If amPm.ToLower().Equals("am") Then
+                                    If hours.ToLower().Equals("12") Then
+                                        hours = "00"
+                                    End If
+                                End If
+                                Dim mDate = Date.Parse(rBook.getDateToReturn())
+                                Dim dets As DateTime = New DateTime(mDate.Year, mDate.Month, mDate.Day, hours, mins, 0)
+                                Dim currentHour = DateTime.Now().Hour
+                                Dim dueHour = Integer.Parse(hours)
+                                Dim hourDiff = dueHour - currentHour
+                                If hourDiff <= 12 Then
+                                    Dim message = String.Format(smsTemplate, rBook.getBorrowerName, rBook.getBookTitle, rBook.getOriginalDateToReturn)
+                                    Try
+                                        Dim result = SMP(smsApiKey, rBook.getContactNumber, Message, "SEMAPHORE")
+                                        insert(String.Format(insertCronSentQuery, rBook.getBookNumber, rBook.getBorrowerName, rBook.getDateToReturn))
+                                    Catch ex As Exception
+                                        ' MsgBox(ex.Message)
+                                    End Try
+                                End If
+
+
+
+                            Catch ex As Exception
+                                MsgBox(ex.Message)
+                            End Try
+                        End If
+
+                    Next
+
+
+                    ' For non printed
+                    Dim nonPrintedList As List(Of ReturnBooks) = getBorrowForNonPrintedData()
+                    For x As Integer = 0 To nonPrintedList.Count - 1
+                        Dim rBook As ReturnBooks = nonPrintedList(x)
+                        If rBook.getContactNumber() = "" Then
+                            Continue For
+                        End If
+                        Dim exist As Boolean = checkIfMsgExist(rBook.getBookNumber(), rBook.getBorrowerName())
+                        If exist Then
+                            Continue For
+                        End If
+
+                        Try
+                            Dim sliceTime = rBook.getOriginalDateToReturn().Split("-")
+                            Dim hoursAndMinutes = sliceTime(1).Split(":")
+                            Dim hours As String = hoursAndMinutes(0).Trim()
+                            Dim sliceMins = hoursAndMinutes(1).Split(" ")
+                            Dim amPm = sliceMins(1).Trim()
+                            Dim mins = sliceMins(0).Trim()
+                            If mins = "00" Then
+                                mins = "00"
+                            End If
+                            If amPm.ToLower().Equals("pm") Then
+                                hours = hours + 12
+                            End If
+
+                            If amPm.ToLower().Equals("am") Then
+                                If hours.ToLower().Equals("12") Then
+                                    hours = "00"
+                                End If
+                            End If
+                            Dim mDate = Date.Parse(rBook.getDateToReturn())
+                            Dim dets As DateTime = New DateTime(mDate.Year, mDate.Month, mDate.Day, hours, mins, 0)
+                            Dim currentHour = DateTime.Now().Hour
+                            Dim dueHour = Integer.Parse(hours)
+                            Dim hourDiff = dueHour - currentHour
+                            If hourDiff = 1 Then
+                                Dim message = String.Format(smsTemplate, rBook.getBorrowerName, rBook.getBookTitle, rBook.getOriginalDateToReturn)
+                                Try
+                                    Dim result = SMP(smsApiKey, rBook.getContactNumber, Message, "SEMAPHORE")
+                                    insert(String.Format(insertCronSentQuery, rBook.getBookNumber, rBook.getBorrowerName, rBook.getDateToReturn))
+                                Catch ex As Exception
+                                    ' MsgBox(ex.Message)
+                                End Try
+                            End If
+
+
+
+                        Catch ex As Exception
+                            MsgBox(ex.Message)
+                        End Try
+
+                    Next
+                End If
+                seconds += 1
+            Loop
+        End If
+    End Sub
+
+    Private Sub cbRunBG_CheckedChanged(sender As Object, e As EventArgs) Handles cbRunBG.CheckedChanged
+        If cbRunBG.CheckState = CheckState.Checked Then
+            runInBG = True
+        Else
+            runInBG = False
+        End If
+    End Sub
+
+    Function SMP(apiKey As String, number As String, message As String, sender As String)
+        Dim responseBody As String
+        Using client As New Net.WebClient()
+            Dim parameter As New Specialized.NameValueCollection
+            Dim url As String = "https://semaphore.co/api/v4/messages"
+            parameter.Add("apikey", apiKey)
+            parameter.Add("number", number)
+            parameter.Add("message", message)
+            parameter.Add("sendername", sender)
+            Dim rpb = client.UploadValues(url, "POST", parameter)
+            responseBody = (New System.Text.UTF8Encoding).GetString(rpb)
+        End Using
+        Return responseBody
+    End Function
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Me.Dispose()
+    End Sub
+End Class
